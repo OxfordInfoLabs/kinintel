a23e6f0e6a8bd695359aca0aff1366bd9a12aeee
Mark Robertshaw <mark@oxil.co.uk>
2021-05-07 14:02:27 +0100

    New S3 data source tests

--- php/test/Objects/Datasource/Amazon/AmazonS3DatasourceTest.php
+++ php/test/Objects/Datasource/Amazon/AmazonS3DatasourceTest.php
@@ -3,11 +3,12 @@
 namespace Kinintel\Objects\Datasource\Amazon;
 
 use Aws\S3\S3Client;
-use Kinikit\Core\Configuration\Configuration;
+use GuzzleHttp\Psr7\Stream;
 use Kinikit\Core\DependencyInjection\Container;
 use Kinikit\Core\Testing\MockObjectProvider;
 use Kinintel\Services\Thirdparty\Amazon\AmazonSDKClientProvider;
 use Kinintel\ValueObjects\Authentication\Generic\AccessKeyAndSecretAuthenticationCredentials;
+use Kinintel\ValueObjects\Dataset\Field;
 use Kinintel\ValueObjects\Datasource\Configuration\Amazon\AmazonS3DatasourceConfig;
 
 include_once "autoloader.php";
@@ -30,15 +31,49 @@ class AmazonS3DatasourceTest extends \PHPUnit\Framework\TestCase {
             ]
         ]]);
 
+
         $mockSDK->returnValue("createS3Client", $mockClient, [
             "eu-west-1", "Hocus", "Pocus"
         ]);
 
+
+        $mockGuzzleStream = MockObjectProvider::instance()->getMockInstance(Stream::class);
+
+        $mockClient->returnValue("getObject", ["Body" => $mockGuzzleStream], [[
+            'Bucket' => "mytestbucket",
+            'Key' => "test.json"
+        ]]);
+
+        $mockGuzzleStream->returnValue("getContents", '[{
+           "name": "Mark",
+           "age": 25
+        },
+        {
+            "name": "Joe",
+            "age": 50
+        }
+        ]');
+
+
         $datasource = new AmazonS3Datasource(new AmazonS3DatasourceConfig("eu-west-1", "mytestbucket", "test.json"),
             new AccessKeyAndSecretAuthenticationCredentials("Hocus", "Pocus"));
 
+
         $result = $datasource->materialiseDataset();
 
+        $this->assertEquals([new Field("name", "Name"), new Field("age", "Age")],
+            $result->getColumns());
+
+        $this->assertEquals([
+            [
+                "name" => "Mark",
+                "age" => 25
+            ],
+            [
+                "name" => "Joe",
+                "age" => 50
+            ]
+        ], $result->getData());
 
     }
 
