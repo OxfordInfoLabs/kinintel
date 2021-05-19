cdf8648d106f5077653c65f20ec18de42866d51a
Mark Robertshaw <mark@oxil.co.uk>
2021-05-19 13:02:42 +0100

    Create Dataset.php

---
+++ php/src/Controllers/Account/Dataset.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Kinintel\Controllers\Account;
+
+
+/**
+ * Dataset controller
+ *
+ * Class Dataset
+ * @package Kinintel\Controllers\Account
+ */
+class Dataset {
+    use \Kinintel\Traits\Controller\Account\Dataset;
+}
