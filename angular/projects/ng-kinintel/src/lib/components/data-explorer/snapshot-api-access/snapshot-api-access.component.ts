b1def2a7e623ea58cdec01b822cad3f92b28ec28
NathanOXIL <44228326+NathanOXIL@users.noreply.github.com>
2024-03-22 10:21:15 +0000

    New snapshot api access saving inline

--- angular/projects/ng-kinintel/src/lib/components/data-explorer/snapshot-api-access/snapshot-api-access.component.ts
+++ angular/projects/ng-kinintel/src/lib/components/data-explorer/snapshot-api-access/snapshot-api-access.component.ts
@@ -3,6 +3,7 @@ import {
     MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA,
     MatLegacyDialogRef as MatDialogRef
 } from '@angular/material/legacy-dialog';
+import {DatasetService} from '../../../services/dataset.service';
 
 @Component({
     selector: 'ki-snapshot-api-access',
@@ -13,12 +14,20 @@ import {
 export class SnapshotApiAccessComponent implements OnInit {
 
     public datasetInstance: any;
+    public showApiAccessDetails = false;
 
     constructor(public dialogRef: MatDialogRef<SnapshotApiAccessComponent>,
-                @Inject(MAT_DIALOG_DATA) public data: any) {
+                @Inject(MAT_DIALOG_DATA) public data: any,
+                private datasetService: DatasetService) {
     }
 
     ngOnInit() {
         this.datasetInstance = this.data.datasetInstanceSummary;
+        this.showApiAccessDetails = !!this.datasetInstance.managementKey;
+    }
+
+    public async saveDataset() {
+        await this.datasetService.saveDataset(this.datasetInstance);
+        this.showApiAccessDetails = !!this.datasetInstance.managementKey;
     }
 }
