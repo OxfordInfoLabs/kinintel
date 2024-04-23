2367a2c92f4998793b925ecefa167f4144dca420
mark@oxil.co.uk <mark@oxil.co.uk>
2024-04-23 08:28:58 +0100

    New Regexp Like functionality

---
+++ php/src/Services/Util/SQLiteFunctions/Regexp.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Kinintel\Services\Util\SQLiteFunctions;
+
+use Kinikit\Core\Logging\Logger;
+use Kinikit\Persistence\Database\Vendors\SQLite3\SQLite3CustomFunction;
+
+class Regexp implements SQLite3CustomFunction {
+
+    public function getName() {
+        return "REGEXP";
+    }
+
+
+    /**
+     * Execute and return a like based on a regexp rhs
+     *
+     * @param mixed ...$arguments
+     * @return mixed
+     */
+    public function execute(...$arguments) {
+        return (sizeof($arguments) == 2) && preg_match('/^' . $arguments[0] . '$/i', $arguments[1]) ? 1 : 0;
+    }
+
+}
