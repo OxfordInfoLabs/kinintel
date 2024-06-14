6a08ef46c6bede5873302112215cf19e379ce0d8
peteroxil <peter@oxil.co.uk>
2024-06-14 14:26:00 +0100

    Allow SQLQuery dataprocessor to take input by filepath

--- php/test/Services/DataProcessor/Query/SQLQueryDataProcessorTest.php
+++ php/test/Services/DataProcessor/Query/SQLQueryDataProcessorTest.php
@@ -20,34 +20,37 @@ include_once "autoloader.php";
 
 class SQLQueryDataProcessorTest extends TestCase {
 
-    /**
-     * @var MockObject
-     */
-    private $authenticationService;
-
-    /**
-     * @var SQLQueryDataProcessor
-     */
-    private $processor;
+    private AuthenticationCredentialsService|MockObject $authenticationService;
+    private SQLQueryDataProcessor|MockObject $processor;
+    private DataProcessorInstance|MockObject $processorInstance;
+    private SQLDatabaseCredentials|MockObject $credentials;
+    private AuthenticationCredentialsInstance|MockObject $credentialsInstance;
+    private DatabaseConnection|MockObject $databaseConnection;
 
     public function setUp(): void {
         $this->authenticationService = MockObjectProvider::instance()->getMockInstance(AuthenticationCredentialsService::class);
         $this->processor = new SQLQueryDataProcessor($this->authenticationService);
+
+        $this->processorInstance = MockObjectProvider::instance()->getMockInstance(DataProcessorInstance::class);
+        $this->credentialsInstance = MockObjectProvider::instance()->getMockInstance(AuthenticationCredentialsInstance::class);
+        $this->authenticationService->returnValue("getCredentialsInstanceByKey", $this->credentialsInstance, ["testKey"]);
+        $this->credentials = MockObjectProvider::instance()->getMockInstance(SQLDatabaseCredentials::class);
+        $this->credentialsInstance->returnValue("returnCredentials", $this->credentials);
+        $this->databaseConnection = MockObjectProvider::instance()->getMockInstance(DatabaseConnection::class);
+        $this->credentials->returnValue("returnDatabaseConnection", $this->databaseConnection);
     }
 
     public function testValidationExceptionThrownIfWrongCredentialsType() {
 
         $query = "SELECT * FROM test";
-        $processorConfig = new SQLQueryDataProcessorConfiguration($query, null, "testKey");
-        $processorInstance = MockObjectProvider::instance()->getMockInstance(DataProcessorInstance::class);
-        $processorInstance->returnValue("returnConfig", $processorConfig);
-        $credentialsInstance = MockObjectProvider::instance()->getMockInstance(AuthenticationCredentialsInstance::class);
-        $this->authenticationService->returnValue("getCredentialsInstanceByKey", $credentialsInstance, ["testKey"]);
+        $processorConfig = new SQLQueryDataProcessorConfiguration(query: $query, authenticationCredentialsKey: "testKey");
+        $this->processorInstance->returnValue("returnConfig", $processorConfig);
+
         $credentials = MockObjectProvider::instance()->getMockInstance(BasicAuthenticationCredentials::class);
-        $credentialsInstance->returnValue("returnCredentials", $credentials);
+        $this->credentialsInstance->returnValue("returnCredentials", $credentials);
 
         try {
-            $this->processor->process($processorInstance);
+            $this->processor->process($this->processorInstance);
             $this->fail("Should have thrown");
         } catch (InvalidDataProcessorConfigException $e) {
             $this->assertTrue(true);
@@ -59,20 +62,12 @@ class SQLQueryDataProcessorTest extends TestCase {
     public function testCanIssueQueryToConfiguredDatabase() {
 
         $query = "SELECT * FROM test";
-        $processorConfig = new SQLQueryDataProcessorConfiguration($query, null, "testKey");
-        $processorInstance = MockObjectProvider::instance()->getMockInstance(DataProcessorInstance::class);
-        $processorInstance->returnValue("returnConfig", $processorConfig);
-        $credentialsInstance = MockObjectProvider::instance()->getMockInstance(AuthenticationCredentialsInstance::class);
-        $this->authenticationService->returnValue("getCredentialsInstanceByKey", $credentialsInstance, ["testKey"]);
-        $credentials = MockObjectProvider::instance()->getMockInstance(SQLDatabaseCredentials::class);
-        $credentialsInstance->returnValue("returnCredentials", $credentials);
-        $databaseConnection = MockObjectProvider::instance()->getMockInstance(DatabaseConnection::class);
-        $credentials->returnValue("returnDatabaseConnection", $databaseConnection);
+        $processorConfig = new SQLQueryDataProcessorConfiguration(query: $query, authenticationCredentialsKey: "testKey");
+        $this->processorInstance->returnValue("returnConfig", $processorConfig);
 
+        $this->processor->process($this->processorInstance);
 
-        $this->processor->process($processorInstance);
-
-        $this->assertTrue($databaseConnection->methodWasCalled("execute", [
+        $this->assertTrue($this->databaseConnection->methodWasCalled("execute", [
             $query
         ]));
     }
@@ -80,52 +75,89 @@ class SQLQueryDataProcessorTest extends TestCase {
     public function testCanEvaluateParameterisedQuery() {
 
         $query = "SELECT * FROM set_{{2_DAYS_AGO | dateConvert 'Y-m-d H:i:s' 'm'}}";
-        $processorConfig = new SQLQueryDataProcessorConfiguration($query, null, "testKey");
-        $processorInstance = MockObjectProvider::instance()->getMockInstance(DataProcessorInstance::class);
-        $processorInstance->returnValue("returnConfig", $processorConfig);
-        $credentialsInstance = MockObjectProvider::instance()->getMockInstance(AuthenticationCredentialsInstance::class);
-        $this->authenticationService->returnValue("getCredentialsInstanceByKey", $credentialsInstance, ["testKey"]);
-        $credentials = MockObjectProvider::instance()->getMockInstance(SQLDatabaseCredentials::class);
-        $credentialsInstance->returnValue("returnCredentials", $credentials);
-        $databaseConnection = MockObjectProvider::instance()->getMockInstance(DatabaseConnection::class);
-        $credentials->returnValue("returnDatabaseConnection", $databaseConnection);
+        $processorConfig = new SQLQueryDataProcessorConfiguration(query: $query, authenticationCredentialsKey: "testKey");
+        $this->processorInstance->returnValue("returnConfig", $processorConfig);
 
-
-        $this->processor->process($processorInstance);
+        $this->processor->process($this->processorInstance);
 
         $month = new \DateTime();
         $month->sub(new \DateInterval("P2D"));
         $expectedQuery = "SELECT * FROM set_" . $month->format("m");
 
-        $this->assertTrue($databaseConnection->methodWasCalled("execute", [
+        $this->assertTrue($this->databaseConnection->methodWasCalled("execute", [
             $expectedQuery
         ]));
     }
 
     public function testCanExecuteMultipleQueries() {
+        $this->assertEmpty($this->databaseConnection->getMethodCallHistory("execute"));
 
         $queries = [
             "SELECT * FROM test",
             "SELECT * FROM test2"
         ];
-        $processorConfig = new SQLQueryDataProcessorConfiguration(null, $queries, "testKey");
-        $processorInstance = MockObjectProvider::instance()->getMockInstance(DataProcessorInstance::class);
-        $processorInstance->returnValue("returnConfig", $processorConfig);
-        $credentialsInstance = MockObjectProvider::instance()->getMockInstance(AuthenticationCredentialsInstance::class);
-        $this->authenticationService->returnValue("getCredentialsInstanceByKey", $credentialsInstance, ["testKey"]);
-        $credentials = MockObjectProvider::instance()->getMockInstance(SQLDatabaseCredentials::class);
-        $credentialsInstance->returnValue("returnCredentials", $credentials);
-        $databaseConnection = MockObjectProvider::instance()->getMockInstance(DatabaseConnection::class);
-        $credentials->returnValue("returnDatabaseConnection", $databaseConnection);
+        $processorConfig = new SQLQueryDataProcessorConfiguration(queries: $queries, authenticationCredentialsKey: "testKey");
+        $this->processorInstance->returnValue("returnConfig", $processorConfig);
+
+        $this->processor->process($this->processorInstance);
 
 
-        $this->processor->process($processorInstance);
-
-        $this->assertTrue($databaseConnection->methodWasCalled("execute", [
+        $this->assertTrue($this->databaseConnection->methodWasCalled("execute", [
             $queries[0]
         ]));
-        $this->assertTrue($databaseConnection->methodWasCalled("execute", [
+        $this->assertTrue($this->databaseConnection->methodWasCalled("execute", [
             $queries[1]
         ]));
+    }
+
+    public function testCanExecuteQueriesFromScriptFile() {
+        $scriptFilepath = "Files/example.sql";
+        $processorConfig = new SQLQueryDataProcessorConfiguration(scriptFilepath: $scriptFilepath, authenticationCredentialsKey: "testKey");
+        $this->processorInstance->returnValue("returnConfig", $processorConfig);
+
+        $this->processor->process($this->processorInstance);
+
+        $expected = [
+            "INSERT INTO table VALUES ('pete # ', 200)",
+            "INSERT INTO table
+
+VALUES
+    (
+        'sam -- ',
+        404
+    )"
+        ];
+        $this->assertEquals(
+            array_map(fn($x) => [$x], $expected),
+            $this->databaseConnection->getMethodCallHistory("execute"));
+    }
+
+    public function testScriptSanitisation(){
+        $sqlScript = <<<EOF
+-- This is an example SQL file with some code that'll be executed. Semicolon for luck! ; cool;
+
+INSERT INTO table VALUES ('pete # ', 200); # Pete exists
+INSERT INTO table
+/**
+  multiline string is here
+ */
+VALUES
+    (
+     'sam -- ',
+     404
+    ); -- Sam is simply a fantasy
+EOF;
+        $statements = SQLQueryDataProcessor::scriptToStatements($sqlScript);
+        $expected = [
+            "INSERT INTO table VALUES ('pete # ', 200)",
+            "INSERT INTO table
+
+VALUES
+    (
+     'sam -- ',
+     404
+    )"
+        ];
+        $this->assertEquals($expected, $statements);
     }
 }
