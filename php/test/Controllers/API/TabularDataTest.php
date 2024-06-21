dcbcc3646fef3266d1ab0132f94863d5d8a4de07
mark@oxil.co.uk <mark@oxil.co.uk>
2024-06-21 15:08:20 +0100

    New List API for data sources

--- php/test/Controllers/API/TabularDataTest.php
+++ php/test/Controllers/API/TabularDataTest.php
@@ -4,8 +4,11 @@ namespace Kinintel\Test\Controllers\API;
 
 use Kinikit\Core\Testing\MockObject;
 use Kinikit\Core\Testing\MockObjectProvider;
+use Kinikit\MVC\Request\Headers;
+use Kinikit\MVC\Request\Request;
 use Kinintel\Controllers\API\TabularData;
 use Kinintel\Exception\DatasourceUpdateException;
+use Kinintel\Objects\Dataset\Tabular\ArrayTabularDataset;
 use Kinintel\Objects\Datasource\DatasourceInstance;
 use Kinintel\Objects\Datasource\SQLDatabase\SQLDatabaseDatasource;
 use Kinintel\Services\Datasource\DatasourceService;
@@ -14,6 +17,8 @@ use Kinintel\ValueObjects\Dataset\Field;
 use Kinintel\ValueObjects\Datasource\Configuration\SQLDatabase\SQLDatabaseDatasourceConfig;
 use Kinintel\ValueObjects\Transformation\Filter\Filter;
 use Kinintel\ValueObjects\Transformation\Filter\FilterJunction;
+use Kinintel\ValueObjects\Transformation\Filter\FilterTransformation;
+use Kinintel\ValueObjects\Transformation\TransformationInstance;
 
 include_once "autoloader.php";
 
@@ -30,6 +35,12 @@ class TabularDataTest extends TestBase {
     private $datasourceService;
 
 
+    /**
+     * @var DatasourceInstance
+     */
+    private $datasourceInstance;
+
+
     public function setUp(): void {
 
         parent::setUp();
@@ -37,12 +48,103 @@ class TabularDataTest extends TestBase {
         $this->datasourceService = MockObjectProvider::instance()->getMockInstance(DatasourceService::class);
         $this->tabularData = new TabularData($this->datasourceService);
 
-        $datasourceInstance = MockObjectProvider::instance()->getMockInstance(DatasourceInstance::class);
-        $datasource = new SQLDatabaseDatasource(new SQLDatabaseDatasourceConfig("table", "test_data", "", [new Field("test"), new Field("test2")]),null,null);
-        $datasourceInstance->returnValue("returnDataSource", $datasource);
+        $this->datasourceInstance = MockObjectProvider::instance()->getMockInstance(DatasourceInstance::class);
+        $datasource = new SQLDatabaseDatasource(new SQLDatabaseDatasourceConfig("table", "test_data", "", [new Field("test"), new Field("test2")]), null, null);
+        $this->datasourceInstance->returnValue("returnDataSource", $datasource);
 
-        $this->datasourceService->returnValue("getDataSourceInstanceByImportKey", $datasourceInstance, ["bingo"]);
+        $this->datasourceService->returnValue("getDataSourceInstanceByImportKey", $this->datasourceInstance, ["bingo"]);
     }
+
+
+    public function testCanListAllDataWithLimitsAndOffsets() {
+
+
+        // Default behaviour - no limits or offsets
+        $arrayDataset = new ArrayTabularDataset([new Field("test")], [["test" => 1]]);
+
+        $request = MockObjectProvider::instance()->getMockInstance(Request::class);
+        $request->returnValue("getParameters", [
+        ]);
+
+
+        $this->datasourceService->returnValue("getEvaluatedDataSource", $arrayDataset, [
+            $this->datasourceInstance, [], [], 0, 100
+        ]);
+
+        $this->assertEquals([["test" => 1]], $this->tabularData->list("bingo", $request));
+
+
+        // Offset and limit defined
+        $arrayDataset = new ArrayTabularDataset([new Field("test")], [["test" => 2]]);
+
+        $request = MockObjectProvider::instance()->getMockInstance(Request::class);
+        $request->returnValue("getParameters", [
+            "offset" => 5,
+            "limit" => 10
+        ]);
+
+
+        $this->datasourceService->returnValue("getEvaluatedDataSource", $arrayDataset, [
+            $this->datasourceInstance, [], [], 5, 10
+        ]);
+
+        $this->assertEquals([["test" => 2]], $this->tabularData->list("bingo", $request));
+
+
+    }
+
+
+    public function testCanFilterListDataWithLimitsAndOffsets() {
+
+
+        // Default behaviour - no limits or offsets
+        $arrayDataset = new ArrayTabularDataset([new Field("test")], [["test" => 1]]);
+
+        $request = MockObjectProvider::instance()->getMockInstance(Request::class);
+        $request->returnValue("getParameters", [
+            "a" => 1,
+            "b" => "hello"
+        ]);
+
+
+        $this->datasourceService->returnValue("getEvaluatedDataSource", $arrayDataset, [
+            $this->datasourceInstance, [], [
+                new TransformationInstance("filter", new FilterTransformation([
+                    new Filter("[[a]]", 1),
+                    new Filter("[[b]]", "hello")
+                ]))
+            ], 0, 100
+        ]);
+
+        $this->assertEquals([["test" => 1]], $this->tabularData->list("bingo", $request));
+
+
+        // Offset and limit defined
+        $arrayDataset = new ArrayTabularDataset([new Field("test")], [["test" => 2]]);
+
+        $request = MockObjectProvider::instance()->getMockInstance(Request::class);
+        $request->returnValue("getParameters", [
+            "a" => 1,
+            "b" => "hello",
+            "offset" => 5,
+            "limit" => 10
+        ]);
+
+
+        $this->datasourceService->returnValue("getEvaluatedDataSource", $arrayDataset, [
+            $this->datasourceInstance, [], [
+                new TransformationInstance("filter", new FilterTransformation([
+                    new Filter("[[a]]", 1),
+                    new Filter("[[b]]", "hello")
+                ]))
+            ], 5, 10
+        ]);
+
+        $this->assertEquals([["test" => 2]], $this->tabularData->list("bingo", $request));
+
+
+    }
+
 
     public function testFilteredDeleteConvertsSimplifiedFilterSyntaxIntoFilterJunction() {
 
@@ -59,7 +161,7 @@ class TabularDataTest extends TestBase {
 
 
         // Advanced filtering
-        $this->tabularData->filteredDelete("bingo", [["column"=> "test", "value" => "55", "matchType" => Filter::FILTER_TYPE_GREATER_THAN], ["column" => "test2", "value" => "Mark", "matchType" => Filter::FILTER_TYPE_LIKE]]);
+        $this->tabularData->filteredDelete("bingo", [["column" => "test", "value" => "55", "matchType" => Filter::FILTER_TYPE_GREATER_THAN], ["column" => "test2", "value" => "Mark", "matchType" => Filter::FILTER_TYPE_LIKE]]);
         $this->assertTrue($this->datasourceService->methodWasCalled("filteredDeleteFromDatasourceInstanceByImportKey", ["bingo",
             new FilterJunction([new Filter("[[test]]", 55, Filter::FILTER_TYPE_GREATER_THAN), new Filter("[[test2]]", "Mark", Filter::FILTER_TYPE_LIKE)])]));
 
