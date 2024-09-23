36ca5dc6f105bdb6b1a3d13118aff8eed13aaad8
NathanOXIL <44228326+NathanOXIL@users.noreply.github.com>
2024-09-23 14:30:41 +0100

    New list option

--- angular/projects/ng-kinintel/src/lib/components/dataset/dataset-editor/dataset-parameter-values/dataset-add-parameter/dataset-add-parameter.component.ts
+++ angular/projects/ng-kinintel/src/lib/components/dataset/dataset-editor/dataset-parameter-values/dataset-add-parameter/dataset-add-parameter.component.ts
@@ -2,6 +2,9 @@ import {Component, Inject, OnInit} from '@angular/core';
 import * as lodash from 'lodash';
 const _ = lodash.default;
 import {MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA, MatLegacyDialogRef as MatDialogRef} from '@angular/material/legacy-dialog';
+import {CreateDatasetComponent} from '../../../../dataset/create-dataset/create-dataset.component';
+import {MatLegacyDialog as MatDialog} from '@angular/material/legacy-dialog';
+import {DatasetService} from '../../../../../services/dataset.service';
 
 @Component({
     selector: 'ki-dataset-add-parameter',
@@ -16,7 +19,9 @@ export class DatasetAddParameterComponent implements OnInit {
     private updateName = true;
 
     constructor(public dialogRef: MatDialogRef<DatasetAddParameterComponent>,
-                @Inject(MAT_DIALOG_DATA) public data: any) {
+                @Inject(MAT_DIALOG_DATA) public data: any,
+                private dialog: MatDialog,
+                private datasetService: DatasetService) {
     }
 
     ngOnInit(): void {
@@ -33,7 +38,17 @@ export class DatasetAddParameterComponent implements OnInit {
     }
 
     public selectDatasource() {
-
+        const dialogRef = this.dialog.open(CreateDatasetComponent, {
+            width: '1200px',
+            height: '800px',
+            data: { }
+        });
+        dialogRef.afterClosed().subscribe(async res => {
+            if (res) {
+                const data = await this.datasetService.evaluateDataset(res, '0', '1');
+                console.log(data);
+            }
+        });
     }
 
 }
