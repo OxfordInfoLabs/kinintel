dc5a2fe5a18f780d121ff914bbfde682cb39b680
PeterSpainOXIL <114655461+PeterSpainOXIL@users.noreply.github.com>
2025-01-09 16:11:40 +0000

    add optional base64 encoding to google cloud creds

--- php/src/ValueObjects/Authentication/Google/GoogleCloudCredentials.php
+++ php/src/ValueObjects/Authentication/Google/GoogleCloudCredentials.php
@@ -11,30 +11,48 @@ class GoogleCloudCredentials implements AuthenticationCredentials {
     /**
      * @var string
      */
-    private $jsonString;
+    private string $jsonString;
+
+    /**
+     * @var bool
+     */
+    private bool $encrypted;
 
     /**
      * GoogleCloudCredentials constructor.
      *
      * @param string $jsonString
      */
-    public function __construct($jsonString) {
+    public function __construct(string $jsonString, bool $encrypted = false) {
         $this->jsonString = $jsonString;
+        $this->encrypted = $encrypted;
     }
 
     /**
      * @return string
      */
-    public function getJsonString() {
-        return $this->jsonString;
+    public function getJsonString(): string {
+
+        if ($this->encrypted) {
+            return base64_decode($this->jsonString);
+        } else {
+            return $this->jsonString;
+        }
     }
 
     /**
      * @param string $jsonString
      */
-    public function setJsonString($jsonString) {
+    public function setJsonString(string $jsonString): void {
         $this->jsonString = $jsonString;
     }
 
+    public function isEncrypted(): bool {
+        return $this->encrypted;
+    }
+
+    public function setEncrypted(bool $encrypted): void {
+        $this->encrypted = $encrypted;
+    }
 
 }
