32767e8c2d469298591fa598310a72859851c067
PeterSpainOXIL <114655461+PeterSpainOXIL@users.noreply.github.com>
2025-07-03 15:50:57 +0100

    tweak command dataprocessor

--- php/src/Services/DataProcessor/Command/CommandDataProcessor.php
+++ php/src/Services/DataProcessor/Command/CommandDataProcessor.php
@@ -16,10 +16,10 @@ class CommandDataProcessor implements DataProcessor {
         /** @var CommandDataProcessorConfiguration $config */
         $config = $instance->returnConfig();
 
-        $success = exec($config->getCommand(), result_code: $resultCode);
+        $success = exec($config->getCommand(), $output, $resultCode);
 
         if ($success == false) {
-            throw new \Exception("Command exited with status code $resultCode");
+            throw new \Exception("Command exited with status code $resultCode. Output:\n" . implode("\n", $output));
         }
 
     }
