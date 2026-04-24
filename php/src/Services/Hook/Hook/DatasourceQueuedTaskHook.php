0123209ee18c50974add60d9cd9b4ba1e979075e
matthewcamusOXIL <matthew.camus@oxil.co.uk>
2026-04-24 16:19:01 +0100

    renamed push api related files fro scheduled tasks to queued tasks

---
+++ php/src/Services/Hook/Hook/DatasourceQueuedTaskHook.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Kinintel\Services\Hook\Hook;
+
+use Kinintel\Exception\UnsupportedDatasetException;
+use Kinintel\Objects\Dataset\Tabular\ArrayTabularDataset;
+use Kinintel\Services\Datasource\DatasourceService;
+use Kinintel\Services\Hook\DatasourceHook;
+use Kinintel\ValueObjects\Hook\Hook\DatasourceQueuedTaskHookConfig;
+use Kiniauth\Services\Workflow\Task\Queued\QueuedTaskService;
+
+class DatasourceQueuedTaskHook implements DatasourceHook {
+
+    /**
+     * @param DatasourceService $datasourceService
+     */
+    public function __construct(
+        private readonly DatasourceService $datasourceService,
+        private readonly QueuedTaskService $queuedTaskService,
+    ) {
+    }
+
+    /**
+     * Get the config class for this datasource hook.
+     *
+     * @return string
+     */
+    public function getConfigClass(): string {
+        return DatasourceQueuedTaskHookConfig::class;
+    }
+
+    /**
+     * Process the hook
+     *
+     * @param DatasourceQueuedTaskHookConfig $hookConfig
+     * @param string $updateMode
+     * @param mixed $updateData
+     *
+     * @return void
+     * @throws UnsupportedDatasetException
+     */
+    public function processHook($hookConfig, $updateMode, $updateData): void {
+
+        //retrieve the data from the hook
+        $data = null;
+        if ($hookConfig->getFields()) {
+            $data = (new ArrayTabularDataset($hookConfig->getFields(), $updateData))->getAllData();
+        } else {
+            $data = $updateData;
+        }
+
+        $newTaskID = $this->queuedTaskService->queueTask(
+            "pushAPIQueue",
+            "pushAPITask",
+            "Push API signals",
+            [
+                "source" => $data[0]["source"] ?? null,
+            ],
+        );
+
+        print_r($this->queuedTaskService->listQueuedTasks("pushAPIQueue"));
+
+        $this->queuedTaskService->processQueuedTask(
+            "pushAPIQueue",
+            "pushAPITask",
+            $newTaskID,
+            [
+                "source" => $data[0]["source"] ?? null,
+            ],
+        );
+
+    }
+}
--- php/src/ValueObjects/Hook/Hook/DatasourceQueuedTaskHookConfig.php
+++ php/src/ValueObjects/Hook/Hook/DatasourceQueuedTaskHookConfig.php
@@ -5,14 +5,13 @@ namespace Kinintel\ValueObjects\Hook\Hook;
 use Kinintel\ValueObjects\Dataset\Field;
 use Kinintel\ValueObjects\Hook\DatasourceHookConfig;
 
-class DatasourceScheduledTaskHookConfig implements DatasourceHookConfig {
+class DatasourceQueuedTaskHookConfig implements DatasourceHookConfig {
 
     /**
      * @param Field[] $fields
      */
     public function __construct(
         public array $fields = [],
-        public array $filters = []
     ) {
     }
 
@@ -28,20 +27,6 @@ class DatasourceScheduledTaskHookConfig implements DatasourceHookConfig {
      */
     public function setFields(array $fields): void {
         $this->fields = $fields;
-    }
-
-    /**
-     * @return array
-     */
-    public function getFilters(): array {
-        return $this->filters;
-    }
-
-    /**
-     * @param array $filters
-     */
-    public function setFilters(array $filters): void {
-        $this->filters = $filters;
     }
 
 }
